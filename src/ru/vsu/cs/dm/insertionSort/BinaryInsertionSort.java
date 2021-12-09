2de9d7ad0395cf0cb4c2b0e0dc52e8e61b85e699
solidus66 <66solidus@gmail.com>
2021-12-10 02:08:20 +0300

    DM_Tasks_3

--- src/ru/vsu/cs/dm/insertionSort/BinaryInsertionSort.java
+++ src/ru/vsu/cs/dm/insertionSort/BinaryInsertionSort.java
@@ -4,15 +4,24 @@ import java.util.Arrays;
 
 public final class BinaryInsertionSort {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
     public static void main(String[] args) {
         //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         //int[] array = {10, 2, 6, 3, 1, 7, 5, 9, 4, 15, 11, 21, 8, 12, 13};
         int[] array = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        System.out.println("The input array: " + Arrays.toString(array));
+        System.out.println();
+        System.out.println(ANSI_CYAN + "The input array: " + ANSI_RESET);
+        System.out.println(Arrays.toString(array));
+        System.out.println();
 
         binaryInsertSort(array);
-        System.out.println("Sorted array: " + Arrays.toString(array));
+        System.out.println();
+
+        System.out.println(ANSI_CYAN + "Sorted array: " + ANSI_RESET);
+        System.out.println(Arrays.toString(array));
     }
 
     static void binaryInsertSort(int[] array) {
